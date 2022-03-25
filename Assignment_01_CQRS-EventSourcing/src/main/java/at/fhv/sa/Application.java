2ba0c2bb20b79a3691660efea9ea60141c8e97de
Smightym8 <michaelspiegel@live.at>
2022-03-25 11:50:35 +0100

    Initial setup

---
+++ Assignment_01_CQRS-EventSourcing/src/main/java/at/fhv/sa/Application.java
@@ -0,0 +1,11 @@
+package at.fhv.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
